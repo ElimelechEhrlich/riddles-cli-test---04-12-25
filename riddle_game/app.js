00c7d0a680e0707de6372d015da4ce5dfc85fb89
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-04 12:59:23 +0200

    game

--- riddle_game/app.js
+++ riddle_game/app.js
@@ -2,6 +2,7 @@ import create_player from "./utils/create_player_object.js"
 import riddles from "./riddles/riddles.js"
 import input from "analiza-sync"
 import check_answer from "./utils/check_answer.js"
+import { time } from "console"
 
 
 
@@ -9,9 +10,10 @@ function app_running() {
     console.log(`Welcome to the Riddle Game!`)
     const name = input("What is your name? ")
     const player = create_player(name)
+    player.times.push(Date.now())
     for (let i = 0; i < riddles.length; i++) {
         check_answer(i)
     }
+    console.log(player)
 }
-
 app_running()
